e5950c7dcaf31dd7dbac1ec5100a715bb198e613
osivff <oscar.fitchfierro@gmail.com>
2025-06-08 10:23:21 -0700

    Final logic; calculate and display amounts

--- script.js
+++ script.js
@@ -2,24 +2,48 @@ let billAmount = document.querySelector('#fbill');
 let tipPercent = document.querySelector('#ftip');
 let tipTotal = document.querySelector('.tip.total-amount');
 let billTotal = document.querySelector('.bill.total-amount');
+let btnCalculate = document.querySelector('#btn-calculate');
 
 let addButton = document.querySelector('#plusButton');
 let subtractButton = document.querySelector('#minusButton');
 
 
-addButton.addEventListener('click', (e)=> {
+addButton.addEventListener('click', ()=> {
     let splitStringPercentage = tipPercent.value.split('%');
     let count = Number(splitStringPercentage[1]);
     let newNumber = `%${count + 1}`
 
-    tipPercent.setAttribute('value', newNumber);
+    tipPercent.value = newNumber;
 }); 
 
-subtractButton.addEventListener('click', (e) => {
+subtractButton.addEventListener('click', () => {
     let splitStringPercentage = tipPercent.value.split('%');
     let count = Number(splitStringPercentage[1]);
     let newNumber = `%${count - 1}`;
-    if(count > 15) {
-        tipPercent.setAttribute('value', newNumber);
+    if(count > 1) {
+        tipPercent.value = newNumber;
     }
 });
+
+btnCalculate.addEventListener('click', () => {
+    let amount = Number(billAmount.value);
+    let tipAmount;
+    let totalAmount;
+    let splitPercentage = tipPercent.value.split('%');
+    let tipNumber = Number(splitPercentage[1]);
+    
+    let displayTip = document.querySelector('#tip-amount');
+    let displayBill = document.querySelector('#bill-amount');
+
+    if(!isNaN(amount) && amount > 0) {
+        tipAmount = (amount/100) * tipNumber;
+        totalAmount = tipAmount + amount;
+    } else {
+        alert('Enter a valid bill and tip amount');
+        return;
+    }
+    
+    displayTip.textContent = `${tipAmount.toFixed(2)}`;
+    displayBill.textContent = `${totalAmount.toFixed(2)}`;
+
+})
